0b5c9e13a2f7a88c0f24c208a82148f5eaf071ee
Simon <simon.coudeville@howest.be>
2019-04-05 11:22:39 +0200

    Refactored the whole bunch

--- themes/coudeville/assets/js/app.js
+++ themes/coudeville/assets/js/app.js
@@ -1,43 +1,41 @@
-function showTopBar() {
-    var topBarObserver = document.querySelector('.js-single-sticky-bar-observer');
-    var topBar = document.querySelector('.js-single-sticky-bar');
+// function showTopBar() {
+//     var topBarObserver = document.querySelector('.js-single-sticky-bar-observer');
+//     var topBar = document.querySelector('.js-single-sticky-bar');
+//
+//     let observer = new IntersectionObserver(function (entries) {
+//         // entries: Array of observed elements
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 topBar.classList.remove("is-shown");
+//             } else {
+//                 topBar.classList.add("is-shown")
+//             }
+//         });
+//     });
+//
+//     observer.observe(topBarObserver);
+// }
 
-    let observer = new IntersectionObserver(function (entries) {
-        // entries: Array of observed elements
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                topBar.classList.remove("is-shown");
-            } else {
-                topBar.classList.add("is-shown")
-            }
-        });
-    });
-
-    observer.observe(topBarObserver);
-}
-
-function showTopTitle() {
-    var topTitleObserver = document.querySelector('.js-single-title');
-    var single = document.querySelector('.js-single');
+function showHeaderTitle() {
+    var introTitle = document.querySelector('.js-intro-title');
+    var body = document.querySelector('body');
 
     var options = {
         rootMargin: '-110px'
-        // rootMargin: '-75px'
-        // rootMargin: '-20px'
     }
 
     let observer = new IntersectionObserver(function (entries) {
         // entries: Array of observed elements
         entries.forEach(entry => {
             if (entry.isIntersecting) {
-                single.classList.remove("has-scrolled");
+                body.classList.remove("has-scrolled");
             } else {
-                single.classList.add("has-scrolled")
+                body.classList.add("has-scrolled")
             }
         });
     }, options);
 
-    observer.observe(topTitleObserver);
+    observer.observe(introTitle);
 }
 
 function toggleBaselines() {
@@ -52,6 +50,6 @@ function toggleBaselines() {
 
 document.addEventListener("DOMContentLoaded", function() {
     toggleBaselines();
-    showTopBar();
-    showTopTitle();
+    // showTopBar();
+    showHeaderTitle();
 });
