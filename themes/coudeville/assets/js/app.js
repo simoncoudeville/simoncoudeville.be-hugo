8bc944dff8c6a683a5fc35dd0b6f81ae33a8c513
Simon <simon.coudeville@howest.be>
2019-06-04 16:31:41 +0200

    Removed old stuff

--- themes/coudeville/assets/js/app.js
+++ themes/coudeville/assets/js/app.js
@@ -1,42 +1,42 @@
-function observeHeader() {
-    var headerObserver = document.querySelector('.js-header-observer');
-    var header = document.querySelector('.js-header');
-
-    let observer = new IntersectionObserver(function (entries) {
-        // entries: Array of observed elements
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                header.classList.remove("is-sticky");
-            } else {
-                header.classList.add("is-sticky")
-            }
-        });
-    });
-
-    observer.observe(headerObserver);
-}
-
-function observeIntroTitle() {
-    var introTitle = document.querySelector('.js-intro-title');
-    var target = document.querySelector('body');
-
-    var options = {
-        rootMargin: '-30px'
-    }
-
-    let observer = new IntersectionObserver(function (entries) {
-        // entries: Array of observed elements
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                target.classList.remove("has-scrolled");
-            } else {
-                target.classList.add("has-scrolled")
-            }
-        });
-    }, options);
-
-    observer.observe(introTitle);
-}
+// function observeHeader() {
+//     var headerObserver = document.querySelector('.js-header-observer');
+//     var header = document.querySelector('.js-header');
+//
+//     let observer = new IntersectionObserver(function (entries) {
+//         // entries: Array of observed elements
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 header.classList.remove("is-sticky");
+//             } else {
+//                 header.classList.add("is-sticky")
+//             }
+//         });
+//     });
+//
+//     observer.observe(headerObserver);
+// }
+// 
+// function observeIntroTitle() {
+//     var introTitle = document.querySelector('.js-intro-title');
+//     var target = document.querySelector('body');
+//
+//     var options = {
+//         rootMargin: '-30px'
+//     }
+//
+//     let observer = new IntersectionObserver(function (entries) {
+//         // entries: Array of observed elements
+//         entries.forEach(entry => {
+//             if (entry.isIntersecting) {
+//                 target.classList.remove("has-scrolled");
+//             } else {
+//                 target.classList.add("has-scrolled")
+//             }
+//         });
+//     }, options);
+//
+//     observer.observe(introTitle);
+// }
 
 function toggleBaselines() {
     const toggles = document.querySelectorAll(".js-toggle-baseline-example");
