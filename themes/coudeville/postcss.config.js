66d2633f699baec59666a6185f70bfdbf714c0c1
Simon <simon.coudeville@howest.be>
2019-06-27 09:49:25 +0200

    Huge refactoring

--- themes/coudeville/postcss.config.js
+++ themes/coudeville/postcss.config.js
@@ -1,6 +1,6 @@
-// module.exports = {
-//     plugins: {
-//         autoprefixer: {
-//         }
-//     },
-// }
+module.exports = {
+    plugins: {
+        autoprefixer: {
+        }
+    },
+}
