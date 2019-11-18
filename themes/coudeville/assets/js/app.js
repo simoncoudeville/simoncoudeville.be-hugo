f322090cad6ca7fa85252b5cd9067d74b50f8e90
Simon <simon.coudeville@howest.be>
2019-11-18 08:42:07 +0100

    Design updates, what else?

--- themes/coudeville/assets/js/app.js
+++ themes/coudeville/assets/js/app.js
@@ -1,6 +1,6 @@
 function observeHeader() {
-    var headerObserver = document.querySelector('.js-header-observer');
-    var header = document.querySelector('.js-header');
+    const headerObserver = document.querySelector('.js-header-observer');
+    const header = document.querySelector('.js-header');
 
     let observer = new IntersectionObserver(function (entries) {
         // entries: Array of observed elements
@@ -17,10 +17,10 @@ function observeHeader() {
 }
 
 function observeIntroTitle() {
-    var title = document.querySelector('.js-title');
-    var target = document.querySelector('body');
+    const title = document.querySelector('.js-title');
+    const target = document.querySelector('body');
 
-    var options = {
+    const options = {
         rootMargin: '-108px'
     }
 
@@ -48,8 +48,23 @@ function toggleBaselines() {
     }
 }
 
+function rotateGradient() {
+    const gradientBackground = document.querySelector('body');
+
+    gradientBackground.addEventListener('mousemove', (e) => {
+        const homeBackgroundImage = document.querySelector('.js-gradient-background-image');
+        const mouseX = (homeBackgroundImage.getBoundingClientRect().left);
+        const mouseY = (homeBackgroundImage.getBoundingClientRect().top);
+        const radianDegrees = Math.atan2(e.pageX - mouseX, e.pageY - mouseY);
+        const rotationDegrees = (radianDegrees * (180/ Math.PI) * -1) + 180;
+        homeBackgroundImage.style.transform = `rotate(${rotationDegrees}deg)`;
+        // homeBackgroundImage.style.setProperty('--deg', rotationDegrees + "deg");
+    });
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     toggleBaselines();
+    rotateGradient();
     // observeHeader();
     // observeIntroTitle();
 });
