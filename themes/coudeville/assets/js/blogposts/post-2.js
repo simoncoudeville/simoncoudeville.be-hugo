918dc1a2412890a9e12a82f2709454d597600ead
Simon <simon.coudeville@howest.be>
2020-07-03 10:21:10 +0200

    Fucking design updates

--- themes/coudeville/assets/js/blogposts/post-2.js
+++ themes/coudeville/assets/js/blogposts/post-2.js
@@ -7,6 +7,32 @@ function toggleBaselines() {
         });
     }
 }
+function hideBaselines() {
+    const buttons = document.querySelectorAll(".js-hide-baseline-button");
+
+    for(b of buttons) {
+        b.addEventListener('click', function() {
+            this.closest(".js-baseline-example").classList.toggle("has-baseline");
+            this.classList.remove('is-enabled');
+            this.setAttribute('aria-pressed', true);
+            this.nextElementSibling.classList.add('is-enabled');
+        });
+    }
+}
+function showBaselines() {
+    const buttons = document.querySelectorAll(".js-show-baseline-button");
+
+    for(b of buttons) {
+        b.addEventListener('click', function() {
+            this.closest(".js-baseline-example").classList.toggle("has-baseline");
+            this.classList.remove('is-enabled');
+            this.setAttribute('aria-pressed', true);
+            this.previousElementSibling.classList.add('is-enabled');
+        });
+    }
+}
 document.addEventListener("DOMContentLoaded", function() {
     toggleBaselines();
+    showBaselines();
+    hideBaselines();
 });
